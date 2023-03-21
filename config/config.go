b8ad9da194c3a14a8d75d28664b21b3757c3bd5b
angga-thetreedots <angga.saputra@thetreedots.com>
2023-03-21 13:31:53 +0700

    feat: setup db railway

--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := "host=localhost user=angga_admin  dbname=petlover port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := "host=containers-us-west-40.railway.app user=postgres  dbname=railway port=7212 password=9tSADWCzD3on9SLPWbTW sslmode=disable TimeZone=Asia/Jakarta"
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
